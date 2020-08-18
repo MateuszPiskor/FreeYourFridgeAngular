40ce1f1eb051e6658f67f5277bd1008fba6c7099
Natalia Filipek <nataliafilipek@MacBook-Air-Natalia.local>
2020-08-18 11:07:21 +0200

    Security

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers {
+    [Authorize]
     [Route ("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase {
@@ -23,8 +25,7 @@ namespace DatingApp.API.Controllers {
             var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
-
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet ("{id}")]
         public async Task<IActionResult> GetValues (int id) {
             var value = await _context.Values.FirstOrDefaultAsync(x => x.Id ==id);
