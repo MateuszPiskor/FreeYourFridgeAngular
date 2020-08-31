80ff660ce9b91acf618560d22d335acd17a7becf
Mateusz Piskor <grytychy@gmail.com>
2020-08-31 12:11:06 +0200

    Add RecipeForDetailDto

---
+++ FreeYourFridge.API/DTOs/RecipeForDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FreeYourFridge.API.DTOs
+{
+    public class RecipeForDetailDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Summary { get; set; }
+        public int ReadyInMinutes { get; set; }
+        public string Instructions { get; set; }
+        public double SpoonacularScore { get; set; }
+        public bool Cheap { get; set; }
+    }
+}
