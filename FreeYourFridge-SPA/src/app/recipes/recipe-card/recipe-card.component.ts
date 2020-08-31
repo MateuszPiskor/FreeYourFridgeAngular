c8b11d5970cf87a97290aa59e8596375d33b78cc
Mateusz Piskor <grytychy@gmail.com>
2020-08-31 11:32:39 +0200

    Add recipe-card.component

---
+++ FreeYourFridge-SPA/src/app/recipes/recipe-card/recipe-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from 'src/app/_models/recipe';
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.scss'],
+})
+export class RecipeCardComponent implements OnInit {
+  @Input() recipe: Recipe;
+  constructor() {}
+
+  ngOnInit() {}
+}
