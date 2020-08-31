d715042028516c4cc476f2ac53e5ab2d7d350c98
Mateusz Piskor <grytychy@gmail.com>
2020-08-31 11:52:01 +0200

    Add recipe controller

---
+++ FreeYourFridge-SPA/src/app/_services/recipe.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from '../_models/recipe';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    'headers': 'headers',
+    'responseType': 'text'
+
+
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  baseUrl = environment.apiUrl;
+constructor(private http: HttpClient) {}
+  getRecipes(): Observable<Recipe[]>{
+    return this.http.get<Recipe[]>(this.baseUrl + 'recipe', httpOptions);
+  }
+
+  getRecipe(id): Observable<Recipe> {
+    return this.http.get<Recipe>(this.baseUrl + 'recipe/' + id, httpOptions);
+  }
+
+  getWidget(id): Observable<string> {
+    return this.http.get<string>(this.baseUrl + 'widget/' + id, httpOptions);
+  }
+
+
+}
