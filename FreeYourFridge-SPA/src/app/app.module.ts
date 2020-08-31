89326922a4cb11174849f05baf057052788cb917
Mateusz Piskor <grytychy@gmail.com>
2020-08-31 11:55:15 +0200

    Add recipe components to app.module.ts

--- FreeYourFridge-SPA/src/app/app.module.ts
+++ FreeYourFridge-SPA/src/app/app.module.ts
@@ -1,54 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import {RouterModule} from '@angular/router';
-import {AuthService} from './_services/auth.service';
+import { RouterModule } from '@angular/router';
+import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
-import {appRoutes} from './routes';
-import {ErrorInterceptorProvider} from './_services/error.interceptor';
+import { appRoutes } from './routes';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { FridgeComponent } from './fridge/fridge.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { FavouredComponent } from './favoured/favoured.component';
 import { MyProfileComponent } from './myProfile/myProfile.component';
 import { ShoppingListComponent } from './shoppingList/shoppingList.component';
 import { DailyMealComponent } from './dailyMeal/dailyMeal.component';
-
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeCardComponent } from './recipes/recipe-card/recipe-card.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
-  declarations: [											
+  declarations: [
     AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent,
-      FooterComponent,
-      ContactComponent,
-      FridgeComponent,
-      RecipesComponent,
-      FavouredComponent,
-      MyProfileComponent,
-      ShoppingListComponent,
-      DailyMealComponent
-   ],
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    FooterComponent,
+    ContactComponent,
+    FridgeComponent,
+    RecipeListComponent,
+    RecipeCardComponent,
+    FavouredComponent,
+    MyProfileComponent,
+    ShoppingListComponent,
+    DailyMealComponent,
+    RecipeDetailComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [
-    ErrorInterceptorProvider,
-    AuthService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ErrorInterceptorProvider, AuthService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
