b5a004addd5a1ead7c520cea7a1b4efd17738691
Mateusz Piskor <grytychy@gmail.com>
2020-08-31 12:14:40 +0200

    Create two new maps in automapper

--- FreeYourFridge.API/Helpers/AutoMapperProfiles.cs
+++ FreeYourFridge.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using AutoMapper;
 using FreeYourFridge.API.DTOs;
 using FreeYourFridge.API.Models;
@@ -9,6 +11,8 @@ namespace FreeYourFridge.API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListDto>();
+            CreateMap<Recipes, RecipeForListDto>().ForMember(dest=>dest.PhotoUrl,opt=>opt.MapFrom(src=>src.Image));
+            CreateMap<Recipes, RecipeForDetailDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Image));
         }
     }
 }
