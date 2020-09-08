a099f246e82944eb2554879125c3a689e11b2a28
Mateusz Piskor <grytychy@gmail.com>
2020-09-08 21:54:21 +0200

    Refactor Instruction and Step class

---
+++ FreeYourFridge.API/Models/Instruction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeYourFridge.API.Models
+{
+    public class Instruction
+    {
+        public string name { get; set; }
+        public Step[] steps { get; set; }
+    }
+}
--- FreeYourFridge.API/Models/Step.cs
+++ FreeYourFridge.API/Models/Step.cs
@@ -1,48 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FreeYourFridge.API.Models
+﻿namespace FreeYourFridge.API.Models
 {
-
-    
-
-    public class Instruction
-    {
-        public string name { get; set; }
-        public Step[] steps { get; set; }
-    }
-
     public class Step
     {
         public int number { get; set; }
         public string step { get; set; }
-        public Ingredient[] ingredients { get; set; }
-        public Equipment[] equipment { get; set; }
-        public Length length { get; set; }
-    }
-
-    public class Length
-    {
-        public int number { get; set; }
-        public string unit { get; set; }
-    }
-
-    public class Ingredient
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string localizedName { get; set; }
-        public string image { get; set; }
-    }
-
-    public class Equipment
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string localizedName { get; set; }
-        public string image { get; set; }
+        
     }
 
 
