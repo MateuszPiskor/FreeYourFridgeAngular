be1c873f127e294344ba75f20249700c468aca2e
Mateusz Piskor <grytychy@gmail.com>
2020-09-08 21:47:49 +0200

    Add apikey reader helper

---
+++ FreeYourFridge.API/Helpers/ApiKeyReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeYourFridge.API.Helpers
+{
+    public class ApiKeyReader
+    {
+        private string _key = System.IO.File.ReadAllText("SpoonacularApiKey.txt");
+        
+        public string getKey()
+        {
+            return _key;
+        }
+
+    }
+}
