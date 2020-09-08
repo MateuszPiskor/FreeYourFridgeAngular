4a8765f921f7058d563fb13d1d1cac3c6f1386ad
Mateusz Piskor <grytychy@gmail.com>
2020-09-08 21:51:03 +0200

    Add urlMaker helper

---
+++ FreeYourFridge.API/Helpers/UrlMaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeYourFridge.API.Models;
+
+namespace FreeYourFridge.API.Helpers
+{
+    public class UrlMaker : IMakePartialUrl
+    {
+        public string UrlIngredientMaker(IEnumerable<Ingredients> ingredients)
+        {
+            string partUrlPath = "";
+            foreach (var ingredient in ingredients)
+            {
+                partUrlPath += ",+" + ingredient.Name;
+            }
+            return partUrlPath.Substring(2);
+        }
+
+    }
+}
