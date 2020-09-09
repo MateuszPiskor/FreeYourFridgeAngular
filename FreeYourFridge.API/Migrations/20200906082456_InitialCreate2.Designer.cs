87a6180d293fddc345f05c72bb01649d1dfa40ec
Mateusz Piskor <grytychy@gmail.com>
2020-09-09 13:35:10 +0200

    Add migration to reaper local db

--- FreeYourFridge.API/Migrations/20200906082456_InitialCreate2.Designer.cs
+++ FreeYourFridge.API/Migrations/20200906082456_InitialCreate2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FreeYourFridge.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FreeYourFridge.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200906082456_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
