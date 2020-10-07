52188623ada497fcddc2745796930b219b116a8e
chashafe <chashafe@gmail.com>
2020-10-07 21:19:37 +0200

    Added test async provider

---
+++ FreeYourFridgeAPI.Tests/TestAsyncProvider/AsyncEnumerable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeYourFridge.API.Tests.TestAsyncProvider
+{
+    public class AsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T> //, IAsyncQueryProvider
+    {
+        public AsyncEnumerable(System.Linq.Expressions.Expression expression)
+            : base(expression) { }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(
+            CancellationToken cancellationToken = new CancellationToken()
+        ) => new AsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+
+    }
+
+    public class AsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public AsyncEnumerator(IEnumerator<T> enumerator) =>
+            _enumerator = enumerator ?? throw new ArgumentNullException();
+
+        public async ValueTask<bool> MoveNextAsync() =>
+            await Task.FromResult(_enumerator.MoveNext());
+
+        public T Current
+        {
+            get => _enumerator.Current;
+        }
+
+        public async ValueTask DisposeAsync()
+            => await Task.FromResult(_enumerator.MoveNext());
+
+    }
+}
---
+++ FreeYourFridgeAPI.Tests/TestAsyncProvider/DataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FreeYourFridge.API.Models;
+
+namespace FreeYourFridge.API.Tests.TestAsyncProvider
+{
+    internal struct DataProvider
+    {
+        internal static IQueryable<DailyMeal> DataDailyMeal { get; } = new List<DailyMeal>
+        {
+            new DailyMeal {Title = "title1", Id = 1, CreatedBy = 11},
+            new DailyMeal {Title = "title2", Id = 2, CreatedBy = 11},
+            new DailyMeal { Title = "title3", Id = 3, CreatedBy = 12},
+            new DailyMeal { Title = "title4", Id = 4, CreatedBy = 12}
+        }.AsQueryable();
+    }
+}
