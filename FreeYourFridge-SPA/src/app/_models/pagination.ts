c5559afd36983bbf2b4a6546f378d6ec14e9e659
Mateusz <alleczad@gmail.com>
2020-10-13 12:05:16 +0200

    Add paging to favoureds

--- FreeYourFridge-SPA/src/app/_models/pagination.ts
+++ FreeYourFridge-SPA/src/app/_models/pagination.ts
@@ -1,13 +1,11 @@
 export interface Pagination {
   currentPage: number;
-  itemsPerPage: number;
-  totalItems: number;
+  pageSize: number;
+  totalCount: number;
   totalPages: number;
 }
 
 export class PaginationResult<T> {
   result: T;
   pagination: Pagination;
-  // static result: import("c:/Users/gryty/OneDrive/Pulpit/programing/codecool/codecool/Advance/ProjectZ/FreeYourFridge-SPA/src/app/_models/Favoured/favouredDto").FavouredDto[];
-  // static pagination: any;
 }
